445207a9a3673c1bf81d1efa3cc47958e41f1175
Ben Hodes <91234329+BlobCanada@users.noreply.github.com>
2026-06-05 22:52:18 -0400

    Added logic to get_prod_servers

--- lab_loops.py
+++ lab_loops.py
@@ -5,8 +5,16 @@ server_list = ['vgh-prod-web01', 'vgh-test-db01', 'tor-prod-app01', 'vgh-prod-db
 
 def get_prod_servers(servers):
     # This function should loop through the servers list and return a NEW list 
-    # containing ONLY servers that have '-prod-' in their hostname.
+    # containing ONLY servers that have 'prod' in their hostname.
     # code here
+    prod_list =  []
+    list_position = 0
+    while list_position < len(servers):
+        temp = [servers[list_position].split('-')]
+        if temp[1] is 'prod':
+            prod_list.append(servers[list_position])
+        list_position += 1
+
 
 def count_vgh_servers(servers):
     # This function should loop through the servers list and COUNT how many 
